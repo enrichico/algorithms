482ab1b0f5f7b6282fe41f06f5d02c98f01f3b8f
enrichico <enrico.casini@gmail.com>
2015-01-29 20:19:45 -0600

    Made struct private
    To prevent users from instantiating a non-initialized collection

--- quickfind/quickfind.go
+++ quickfind/quickfind.go
@@ -1,44 +1,43 @@
-// Package quickfind contains different approaches to solve the (union find) dynamic connectivity problem
+// Package collection contains different approaches to solve the (union find) dynamic connectivity problem
 package quickfind
 
 import "fmt"
 
-// QuickFind is an implementation of the QuickFind algorithm (eager approch) to resolve the dynamic connectivity problem (union find).
-type QuickFind struct {
+type collection struct {
 	id []int
 }
 
-func New(n int) *QuickFind {
+func New(n int) *collection {
 
-	qf := new(QuickFind)
+	c := new(collection)
 
-	qf.id = make([]int, n)
+	c.id = make([]int, n)
 	//initialize slice
 	for i := 0; i < n; i++ {
-		qf.id[i] = i
+		c.id[i] = i
 	}
 
-	return qf
+	return c
 }
 
-func (qf *QuickFind) Connected(p int, q int) bool {
+func (c *collection) Connected(p int, q int) bool {
 
-	return qf.id[p] == qf.id[q]
+	return c.id[p] == c.id[q]
 }
 
-func (qf *QuickFind) Union(p int, q int) {
+func (c *collection) Union(p int, q int) {
 
-	pid := qf.id[p]
-	qid := qf.id[q]
+	pid := c.id[p]
+	qid := c.id[q]
 
-	for i := 0; i < len(qf.id); i++ {
-		if qf.id[i] == pid {
-			qf.id[i] = qid
+	for i := 0; i < len(c.id); i++ {
+		if c.id[i] == pid {
+			c.id[i] = qid
 		}
 	}
 }
 
-func (qf *QuickFind) PrintIds() {
+func (c *collection) PrintIds() {
 
-	fmt.Printf("%v\n", qf.id)
+	fmt.Printf("%v\n", c.id)
 }
