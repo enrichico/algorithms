5563b766230d8fea9dd4ecc0a0463a8b7d938d1c
enrichico <enrico.casini@gmail.com>
2015-02-05 18:22:59 -0600

    Renamed package 'stack' to 'stringstack'
    Changes package name to fit the ‘go way’

---
+++ stringstack/stack.go
@@ -0,0 +1,49 @@
+//Package stringstack contains implementation of a stack of strings
+package stringstack
+
+import "fmt"
+
+type node struct {
+	item string
+	next *node
+}
+
+//Type Stack provides the implementation of a stack of strings as a linked list
+type Stack struct {
+	first *node
+}
+
+func New() *Stack {
+
+	ss := new(Stack)
+	return ss
+}
+
+func (s *Stack) IsEmpty() bool {
+
+	return (s.first == nil)
+}
+
+func (s *Stack) Push(item string) {
+
+	oldFirst := s.first
+	s.first = new(node)
+	s.first.item = item
+	s.first.next = oldFirst
+}
+
+func (s *Stack) Pop() string {
+
+	item := s.first.item
+	s.first = s.first.next
+	return item
+}
+
+func (s *Stack) Print() {
+
+	for e := s.first; e != nil; e = e.next {
+		// do something with e.item
+		fmt.Printf("%s\n", e.item)
+	}
+
+}
